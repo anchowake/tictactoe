9640926c563c784274dc11b2ee7ffc166f8e4c2c
Ruben Polanco <ren.suga@gmail.com>
2017-03-15 21:14:35 -0600

    Creates Logic for the game: winner, draw, turns, invalid moves

--- src/Announcement.jsx
+++ src/Announcement.jsx
@@ -3,8 +3,13 @@ import React, { Component } from 'react';
 export default class Announcement extends Component {
 	render(){
 		return(
-			<div className={this.props.winner ? 'visible' : 'hidden'}>
-				<h2>Game Over - </h2>
+			<div>
+				<h2 className={this.props.winner ? 'visible' : 'hidden'}>
+					{(this.props.winner == 'draw') ? this.props.winner : 'The winner is: ' + this.props.winner}
+				</h2>
+				<h2 className={this.props.winner ? 'hidden' : ''}>
+					{this.props.invalid ? this.props.invalid : 'Current Turn: ' + this.props.turn}
+				</h2>
 			</div>
 		)
 	}
--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-{/* Imports components */}
 import Announcement from './Announcement.jsx';
 import ResetButton from './ResetButton.jsx';
 import Tile from './Tile.jsx';
@@ -9,20 +8,71 @@ import Tile from './Tile.jsx';
 class App extends Component {
   constructor(){
     super();
-    {/* Defines the state of the board */}
-    this.state = {
+
+    this.state={
       gameBoard: [
         ' ',' ',' ',
         ' ',' ',' ',
         ' ',' ',' '
       ],
       turn: 'x',
-      winner: null
+      winner: null,
+      invalid: null
     }
   }
 
   updateBoard(loc, player){
+    if(!this.state.winner){
+      if(this.state.gameBoard[loc] === 'x' || this.state.gameBoard[loc] === 'o'){
+      {/* Sets Invalid Move State */}
+        this.setState({invalid: 'Invalid Move'});
+        return;  
+      }else{
+        {/* Resets Invalid Move State */}
+        this.setState({invalid: null});
+        {/* Place play in the board */}
+        let currentGameBoard = this.state.gameBoard;
+        currentGameBoard.splice(loc, 1, this.state.turn);
+        this.setState({gameBoard: currentGameBoard});
 
+        {/* Changes the player turn */}
+        this.setState({turn:(this.state.turn === 'x') ? 'o' : 'x'});
+
+        {/* Check ROWS  to see if there's a winner */}
+        for (let i = 0; i <= 2; i+=3){
+            let rowsContent = this.state.gameBoard[i] + this.state.gameBoard[i+1] + this.state.gameBoard[i+2];
+            if(rowsContent.match(/xxx|ooo/)){
+                this.setState({winner:this.state.turn});
+            }
+        }
+
+        {/* Check COLUMNS to see if there's a winner*/}
+        for (let i = 0; i <= 2; i+=1){
+            let colsContent = this.state.gameBoard[i] + this.state.gameBoard[i+3] + this.state.gameBoard[i+6];
+            if(colsContent.match(/xxx|ooo/)){
+                this.setState({winner:this.state.turn});
+            }
+        }
+
+        {/* Check LEFT DIAGONAL to see if there's a winner */}
+        let leftDiagonal = this.state.gameBoard[0] + this.state.gameBoard[4] + this.state.gameBoard[8];
+        if(leftDiagonal.match(/xxx|ooo/)){
+            this.setState({winner:this.state.turn});
+        }
+
+        {/* Check RIGHT DIAGONAL to see if there's a winner */}
+        let rightDiagonal = this.state.gameBoard[2] + this.state.gameBoard[4] + this.state.gameBoard[6];
+        if(rightDiagonal.match(/xxx|ooo/)){
+            this.setState({winner:this.state.turn});
+        }
+
+        {/* Check if it's a Draw */}
+        let movesDone = this.state.gameBoard.join('').replace(/ /g, '');
+        if(movesDone.length === 9 && !this.state.winner){
+          this.setState({winner: 'draw'});
+        }
+      }
+    }
   }
 
   resetBoard(){
@@ -33,27 +83,28 @@ class App extends Component {
         ' ',' ',' '
       ],
       turn: 'x',
-      winner: null
+      winner: null,
+      invalid: null
     })
   }
 
   render() {
     return (
       <div className="container">
-        {/* Header */}
         <div className="menu">
-          <h1>Tic Tac Toe - React</h1>
-          <Announcement 
-            winner = {this.state.winner}
+          <h1>React Exercise</h1>
+          <Announcement
+            winner={this.state.winner}
+            invalid={this.state.invalid}
+            turn={this.state.turn}
           />
-          <ResetButton 
-            reset = {this.resetBoard.bind(this)}
+          <ResetButton
+            reset={this.resetBoard.bind(this)}
           />
         </div>
-        {/* Maps the gameBoard and retrieves value and index */}
         {this.state.gameBoard.map(function(value,i){
           return (
-            <Tile 
+            <Tile
               key={i}
               loc={i}
               value={value}
